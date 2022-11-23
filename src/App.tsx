0fbb50578816d0c98b4c6616798c4e85d9b89e7a
John Brunton <john_brunton@live.co.uk>
2022-11-23 22:11:39 +0000

    test: add jest

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,7 @@
 import { useEffect, useState } from "react";
 import { FormattedNumber } from "react-intl";
 import { Input } from "./components/Input";
-
-type Params = {
-  loan: number;
-  rate: number;
-  term: number;
-};
-
-type Repayment = {
-  month: number;
-  principal: number;
-  monthlyRepayment: number;
-  monthlyInterest: number;
-  cumulativeInterest: number;
-};
-
-type RepaymentsSummary = {
-  repayments: Repayment[];
-  totalCost: number;
-};
-
-const calculateRepayments = ({
-  loan,
-  rate,
-  term,
-}: Params): RepaymentsSummary => {
-  let principal = loan;
-  let month = 0;
-  let cumulativeInterest = 0;
-
-  const monthlyRate = rate / 100 / 12;
-
-  const repayments: Repayment[] = [];
-
-  while (principal > 0.01) {
-    ++month;
-
-    const remainingTerm = term * 12 - month;
-
-    const ln = Math.pow(1 + monthlyRate, remainingTerm);
-    const monthlyRepayment = principal / ((1 - 1 / ln) / monthlyRate);
-
-    const monthlyInterest = principal * monthlyRate;
-    cumulativeInterest += monthlyInterest;
-
-    repayments.push({
-      month,
-      principal,
-      monthlyRepayment,
-      monthlyInterest,
-      cumulativeInterest,
-    });
-
-    principal -= monthlyRepayment - monthlyInterest;
-  }
-
-  return {
-    repayments,
-    totalCost: cumulativeInterest,
-  };
-};
+import { calculateRepayments, RepaymentsSummary } from "./repayments";
 
 function App() {
   const [loan, setLoan] = useState(100_000);
