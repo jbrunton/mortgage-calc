e2f0a0bb490c4a542b250c4b45b0922ecf34888a
John Brunton <john_brunton@live.co.uk>
2022-12-02 21:31:59 +0000

    feat: show repayments summary in scenarios list

--- src/domain/entities/scenarios.ts
+++ src/domain/entities/scenarios.ts
@@ -1,6 +1,8 @@
+import { RepaymentsSummary } from "@usecases/repayments/calculate_repayments";
 import { Params } from "./repayments";
 
 export type Scenario = {
   params: Params;
+  summary: Pick<RepaymentsSummary, "monthlyAmount" | "totalInterest">;
   description: string;
 };
