d597c037baee44ebef8e3e47f43db32a8c382c11
John Brunton <john_brunton@live.co.uk>
2024-01-01 16:03:21 +0000

    test: improve test coverage

--- src/domain/usecases/mortgages/calculate_repayments.spec.ts
+++ src/domain/usecases/mortgages/calculate_repayments.spec.ts
@@ -24,38 +24,69 @@ describe("calculateRepayments", () => {
     expect(repayments.length).toEqual(24);
   });
 
-  describe("calculates stamp duty", () => {
+  describe("calculates the cost of loan", () => {
+    // checked against https://www.experian.co.uk/consumer/mortgages/calculators/mortgage-calculator.html on 1/1/24
     const examples = [
       {
-        propertyValue: 250_000,
-        stampDuty: 0,
-        firstTimeBuyer: false,
+        loan: 100_000,
+        rate: 3.5,
+        term: 20,
+        monthlyAmount: 580,
+        totalInterest: 39_190,
+        totalRepayment: 139_190,
       },
       {
-        propertyValue: 500_000,
-        stampDuty: 12_500,
-        firstTimeBuyer: false,
+        loan: 200_000,
+        rate: 4.5,
+        term: 25,
+        monthlyAmount: 1_112,
+        totalInterest: 133_499,
+        totalRepayment: 333_499,
       },
       {
-        propertyValue: 1_000_000,
-        stampDuty: 41_250,
-        firstTimeBuyer: false,
+        loan: 250_000,
+        rate: 5.5,
+        term: 30,
+        monthlyAmount: 1_419,
+        totalInterest: 261_010,
+        totalRepayment: 511_010,
       },
-      {
-        propertyValue: 250_000,
-        stampDuty: 0,
-        firstTimeBuyer: true,
+    ];
+
+    examples.forEach(
+      ({ loan, rate, term, monthlyAmount, totalInterest, totalRepayment }) => {
+        test(`cost of ${
+          loan / 1000
+        }k loan is ${totalInterest} when rate=${rate}, term=${term}`, () => {
+          const summary = calculateRepayments({
+            propertyValue: 500_000,
+            firstTimeBuyer: false,
+            loan,
+            rate,
+            term,
+          });
+          expect(summary.monthlyAmount).toBeCloseTo(monthlyAmount, 0);
+          expect(summary.totalInterest).toBeCloseTo(totalInterest, 0);
+          expect(summary.totalRepayment).toBeCloseTo(totalRepayment, 0);
+        });
       },
-      {
-        propertyValue: 500_000,
-        stampDuty: 3_750,
-        firstTimeBuyer: true,
-      },
-      {
-        propertyValue: 1_000_000,
-        stampDuty: 41_250,
-        firstTimeBuyer: true,
-      },
+    );
+  });
+
+  describe("calculates stamp duty", () => {
+    // checked against https://www.moneysavingexpert.com/mortgages/stamp-duty/ on 1/1/24
+    const examples = [
+      { propertyValue: 250_000, firstTimeBuyer: false, stampDuty: 0 },
+      { propertyValue: 500_000, firstTimeBuyer: false, stampDuty: 12_500 },
+      { propertyValue: 600_000, firstTimeBuyer: false, stampDuty: 17_500 },
+      { propertyValue: 700_000, firstTimeBuyer: false, stampDuty: 22_500 },
+      { propertyValue: 1_000_000, firstTimeBuyer: false, stampDuty: 41_250 },
+      { propertyValue: 1_500_000, firstTimeBuyer: false, stampDuty: 91_250 },
+      { propertyValue: 2_000_000, firstTimeBuyer: false, stampDuty: 151_250 },
+      { propertyValue: 250_000, firstTimeBuyer: true, stampDuty: 0 },
+      { propertyValue: 500_000, firstTimeBuyer: true, stampDuty: 3_750 },
+      { propertyValue: 600_000, firstTimeBuyer: true, stampDuty: 8_750 },
+      { propertyValue: 700_000, firstTimeBuyer: true, stampDuty: 22_500 },
     ];
 
     examples.forEach(({ propertyValue, stampDuty, firstTimeBuyer }) => {
